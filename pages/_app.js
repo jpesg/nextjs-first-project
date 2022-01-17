c3aedc95ab4e6814563b91d97105b389ab02e7ae
Javier.Pesado <javier.pesado@insurancerevolution.co.uk>
2022-01-17 21:10:08 +0000

    merging head content

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 import Head from "next/head";
 function MyApp({ Component, pageProps }) {
+  /*NextJs merge Head elementes into one, persists last one */
   return (
     <Layout>
       <Head>
+        <title>NextJs Events</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-with" />
       </Head>
       <Component {...pageProps} />
